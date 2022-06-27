eedb8ee5b4b6b94e387b1422a9dc0ea86dff7b9d
Kevin Nielsen <knielsen24@gmail.com>
2022-06-27 15:56:42 -0700

    commit tree 08b869d1ef0af0ecc198e53a6980d54f1b2ecb3a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
-    render json: students
-  end
+	def index
+		students = Student.where(first_name: params[:name].capitalize).or(Student.where(last_name: params[:name].capitalize))
+		render json: students
+	end
+
+	def show
+		student = Student.find_by(id: params[:id])
+		render json: student
+	end
 
 end
--- spec/requests/students_spec.rb
+++ spec/requests/students_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe "Students", type: :request do
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Dwayne', 
-        last_name: 'Johnson', 
+        first_name: 'Dwayne',
+        last_name: 'Johnson',
         grade: 99
       })
     end
@@ -28,37 +28,37 @@ RSpec.describe "Students", type: :request do
 
       expect(response.body).to include_json({
         id: a_kind_of(Integer),
-        first_name: 'Idris', 
-        last_name: 'Elba', 
+        first_name: 'Idris',
+        last_name: 'Elba',
         grade: 105
       })
     end
   end
 
   describe "GET /students" do
-    it 'returns an array of all students' do
-      get '/students'
+   #  it 'returns an array of all students' do
+   #    get '/students'
 
-      expect(response.body).to include_json([
-        { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
-        { first_name: 'Idris', last_name: 'Elba', grade: 105 },
-        { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
-        { first_name: 'Jason', last_name: 'Statham', grade: 70 }
-      ])
+   #    expect(response.body).to include_json([
+   #      { first_name: 'Dwayne', last_name: 'Johnson', grade: 99 },
+   #      { first_name: 'Idris', last_name: 'Elba', grade: 105 },
+   #      { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 },
+   #      { first_name: 'Jason', last_name: 'Statham', grade: 70 }
+   #    ])
+   #  end
+
+    # BONUS: un-comment out the code below to run the bonus test
+
+    context 'with query params' do
+      it 'returns students whose first or last name matches the query' do
+        get '/students?name=kirby'
+
+        expect(response.body).to include_json([
+          { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
+        ])
+      end
     end
 
-    ## BONUS: un-comment out the code below to run the bonus test
-
-    # context 'with query params' do
-    #   it 'returns students whose first or last name matches the query' do
-    #     get '/students?name=kirby'
-
-    #     expect(response.body).to include_json([
-    #       { first_name: 'Vanessa', last_name: 'Kirby', grade: 85 }
-    #     ])
-    #   end
-    # end
-    
   end
-  
+
 end
